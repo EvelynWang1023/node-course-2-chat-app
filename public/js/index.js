f84bec569ff7b08f3005d69f8e3679ed594ed415
Wang <changwang.tracy@gmail.com>
2018-04-09 13:58:03 -0700

    Emit newMessage on createMessage

--- public/js/index.js
+++ public/js/index.js
@@ -2,17 +2,12 @@ var socket = io();
             
 socket.on('connect', function(){ 
     console.log('Connected to server.');
-
-    socket.emit('createMessage', {
-        from: 'Andrew',
-        text: 'Hey, this is Andrew'
-    });
-});
-
-socket.on('disconnect', function() {  
-    console.log('Disconnected to server');
 });
 
 socket.on('newMessage', function(message) {
     console.log('newMessage', message);
+});
+
+socket.on('disconnect', function() {  
+    console.log('Disconnected to server');
 });
